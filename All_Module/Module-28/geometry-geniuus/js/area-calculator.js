ba8d544595ea358d5f6b57ad37811d4512efaa60
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-18 12:25:28 +0600

    area calculation

--- All_Module/Module-28/geometry-geniuus/js/area-calculator.js
+++ All_Module/Module-28/geometry-geniuus/js/area-calculator.js
@@ -10,4 +10,29 @@ function calculateTriangleArea(){
     const heightValueString = heightField.value;
     const height = parseFloat(heightValueString);
     console.log(height);
+
+    const area = 0.5 * base * height;
+    
+    //show traingle area
+    const areaSpan = document.getElementById('traingle-area');
+    areaSpan.innerText = area;
+}
+
+function calculateRectangleArea(){
+    //get rectangle width
+    const widthField = document.getElementById('rectangle-width');
+    const widthvalueString = widthField.value;
+    const width = parseFloat(widthvalueString);
+    
+    //get rectangle length value
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueString = lengthField.value;
+    const length = parseFloat(lengthValueString);
+
+    const area = width * length;
+    
+    //show rectangle area
+    const areaSpan = document.getElementById('rectangle-area');
+    areaSpan.innerText = area;
+    
 }
