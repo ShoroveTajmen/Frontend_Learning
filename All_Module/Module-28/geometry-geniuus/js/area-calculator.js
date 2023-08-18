39a1e367620ea52b369964b90194e569bbc1d2db
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-19 00:23:29 +0600

    create dynamic HTML

--- All_Module/Module-28/geometry-geniuus/js/area-calculator.js
+++ All_Module/Module-28/geometry-geniuus/js/area-calculator.js
@@ -1,3 +1,4 @@
+//Traingle---------------------
 function calculateTriangleArea(){
     //get traingle base value
     const baseFiled = document.getElementById('traingle-base');
@@ -16,11 +17,11 @@ function calculateTriangleArea(){
     //show traingle area
     const areaSpan = document.getElementById('traingle-area');
     areaSpan.innerText = area;
+    addToCalculationEntry('Traigle', area);
 }
 
 
 //Rectangle------------------
-
 function calculateRectangleArea(){
     //get rectangle width
     const widthField = document.getElementById('rectangle-width');
@@ -32,6 +33,7 @@ function calculateRectangleArea(){
     const lengthValueString = lengthField.value;
     const length = parseFloat(lengthValueString);
 
+
     //vvalidate input
     if(isNaN(width) || isNaN(length)){
         alert('please insert a number');
@@ -42,11 +44,14 @@ function calculateRectangleArea(){
     //show rectangle area
     const areaSpan = document.getElementById('rectangle-area');
     areaSpan.innerText = area;
+
+    addToCalculationEntry('Rectangle', area);
     
 }
 
-//reusable function ---> reduce duplicate code
 
+//Parallelogrm------------------------
+//reusable function ---> reduce duplicate code
 function calculateParallelogramArea(){
    const base = getInputValue('Parallelogram-base');
    const height = getInputValue('Parallelogram-height');
@@ -58,6 +63,9 @@ function calculateParallelogramArea(){
    }
    const area = base * height;
    setElementInnerText('Parallelogram-area', area);
+
+   //add to clcuultion entry
+   addToCalculationEntry('Parallelogrm', area)
    
 }
 
@@ -70,7 +78,10 @@ function calculateEllipseArea(){
     const areaDecimal = area.toFixed(2);
     console.log(areaDecimal);
     setElementInnerText('ellipse-area', areaDecimal);
+
+    addToCalculationEntry('Ellipse', areaDecimal)
 }
+
 
 //reusable get input value field in numberr
 function getInputValue(fieldId){
@@ -81,6 +92,25 @@ function getInputValue(fieldId){
 }
 
 //add to calculation entry
+/*
+1. get the element where you want to add the dynamic HTML
+2. create a element you want to add
+3. if needed add some class
+4. set inner HTML. it could be dynmic template string
+5.append the created element as a child of the parent
+*/
+function addToCalculationEntry(areaType, area){
+    console.log(areaType + ' ' + area);
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;;
+
+    const p = document.createElement('p');
+    p.classList.add('my-4')
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;
+
+    calculationEntry.appendChild(p);
+}
 
 
 //reusable set span, p , div, etc text
