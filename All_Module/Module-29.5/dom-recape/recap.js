929db908837a586dfbcab28fdea56cd56df85ee2
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-21 17:38:43 +0600

    js recap and validation and date

---
+++ All_Module/Module-29.5/dom-recape/recap.js
@@ -0,0 +1,24 @@
+/*
+. how to use js with html and csss
+2. get elementt or elements from dom
+  - getElementById
+  -getElementByClssName
+  -querySelector
+  -querySelectorAll
+
+
+3. dynamically add element to the dom : document.createElement
+   - set value to the innerHTML
+
+4. get and set value from/to a dom element
+   -use innerText (h1, p, h5, div, span , strong)
+   -use value (inpuut, texxtAreaaa)
+   
+5. add event listener (event handler)
+   - click, focus, blur, onChange, keyuup
+   - addEventListener onclick = "myHandler()" 
+   
+6. use function to reduce duplicate code
+7. event bubble and event delegate   
+   
+*/
---
+++ All_Module/Module-29.5/dom-recape/validation.js
@@ -0,0 +1,12 @@
+const number = 23;
+//string, number, boolean
+if(typeof number === 'number'){
+    console.log('value is a number')
+}else{
+    console.log('value is not a number');
+}
+
+const numbers = [3, 12, 778, 90];
+console.log(Array.isArray(numbers));
+const student = {name:'shorove', age:23, job:'kkkkkk'};
+console.log(typeof student);
