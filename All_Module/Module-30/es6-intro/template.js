55ea0c2335cf1a24c3249c37456d0e4096c9697c
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-23 15:22:10 +0600

    temlate literals

---
+++ All_Module/Module-30/es6-intro/template.js
@@ -0,0 +1,19 @@
+const first = 'jaan';
+const last = 'pakhi';
+const greet = 'potas potas';
+
+const name = first + ' ' + last + ' ' + greet;
+console.log(name);
+
+//let's using template string
+const a = 10;
+const b = 10;
+
+const result = `the sum of ${a} and ${b} is ${a+b}`;
+console.log(result);
+
+//its easy to move next line using temlate stringg
+const summary = `my name is shorove
+i am 23 years old
+i am studying css`;
+console.log(summary);
