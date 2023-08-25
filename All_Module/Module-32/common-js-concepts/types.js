c565f8090043a06d1dab99fa8d3896c4c517bc79
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-25 20:09:52 +0600

    various types of js

---
+++ All_Module/Module-32/common-js-concepts/types.js
@@ -0,0 +1,18 @@
+//js is dynamic programming laanguaaage 
+
+// data type (primitive)
+const a = 5;
+const b = 'shorove tajmen';
+const c = true;
+// console.log(typeof a, typeof b, typeof c, typeof ages, typeof student);
+
+
+//non-primitive data type
+const ages = [45,65,48];
+const student = {name: 'shorove', class:12};
+
+let x = 5;
+let y = x;
+console.log(x,y);
+y = 7;
+console.log(x,y);
