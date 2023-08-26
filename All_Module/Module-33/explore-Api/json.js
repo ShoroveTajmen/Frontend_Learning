88f4bc58f6a159ef491ea1cead08b06652c9dcb7
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-26 19:49:52 +0600

    json stringify

---
+++ All_Module/Module-33/explore-Api/json.js
@@ -0,0 +1,10 @@
+const user = {id: 1, name: 'Gorib Aamir', job: 'actor'};
+//javaScript object notation {JSON}
+
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
+
+/*
+{ id: 1, name: 'Gorib Aamir', job: 'actor' }
+{"id":1,"name":"Gorib Aamir","job":"actor"} */
