61bf7a28c07f259cf101bed674aeb34c8adec9d5
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-26 21:21:20 +0600

    json stringify

--- All_Module/Module-33/explore-Api/json.js
+++ All_Module/Module-33/explore-Api/json.js
@@ -2,8 +2,28 @@ const user = {id: 1, name: 'Gorib Aamir', job: 'actor'};
 //javaScript object notation {JSON}
 
 const stringified = JSON.stringify(user);
-console.log(user);
-console.log(stringified);
+// console.log(user);
+// console.log(stringified);
+
+
+const shop = {
+    owner: 'alia',
+    address: {
+        stret: 'kochukhet voot er goli',
+        city: 'ranbir',
+        country: 'BD'
+    },
+    products : ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+};
+console.log(shop);
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+//if we want to again back to the main object from json string then
+const jsonObj = JSON.parse(shopJSON);
+console.log(jsonObj);
 
 /*
 { id: 1, name: 'Gorib Aamir', job: 'actor' }
