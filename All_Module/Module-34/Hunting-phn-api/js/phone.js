39a4f1abfa265396a79d59b3bc7e18aa5d707d40
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-28 04:12:19 +0600

    add spinner or loader

--- All_Module/Module-34/Hunting-phn-api/js/phone.js
+++ All_Module/Module-34/Hunting-phn-api/js/phone.js
@@ -12,9 +12,20 @@ const displayPhones = (phones) => {
   // console.log(phones);
 
   //1. get the element
-  const phoneContainer = document.getElementById('phone-container');
+  const phoneContainer = document.getElementById("phone-container");
   //clear phone container cards before adding new cards
-  phoneContainer.textContent = '';
+  phoneContainer.textContent = "";
+
+  //display show all button if there are more than 12 phones
+  const showAllContainer = document.getElementById("show-all-container");
+  if (phones.length > 12) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
+
+  //display only first 12 phones
+  phones = phones.slice(0, 12);
 
   phones.forEach((phone) => {
     // console.log(phone);
@@ -37,18 +48,29 @@ const displayPhones = (phones) => {
               </div>
             </div>
         `;
-        //4. append child
-        phoneContainer.appendChild(phoneCard);
-        
+    //4. append child
+    phoneContainer.appendChild(phoneCard);
   });
+  //hide loading spinner
+  toggleLoadingSpinner(false);
 };
 
 // handle search button
 const handleSearch = () => {
-   const searchField = document.getElementById('search-field');
-   const searchText = searchField.value;
-   console.log(searchText);
-   loadPhone(searchText);
-}
+  toggleLoadingSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  //    console.log(searchText);
+  loadPhone(searchText);
+};
 
-loadPhone();
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
+
+// loadPhone();
