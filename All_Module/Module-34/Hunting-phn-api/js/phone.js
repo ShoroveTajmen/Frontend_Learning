81131c4118e512f94df4a9d5f8d0946ed62a9530
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-28 03:00:43 +0600

    add search functionality

--- All_Module/Module-34/Hunting-phn-api/js/phone.js
+++ All_Module/Module-34/Hunting-phn-api/js/phone.js
@@ -1,6 +1,6 @@
-const loadPhone = async () => {
+const loadPhone = async (searchText) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
@@ -13,12 +13,14 @@ const displayPhones = (phones) => {
 
   //1. get the element
   const phoneContainer = document.getElementById('phone-container');
+  //clear phone container cards before adding new cards
+  phoneContainer.textContent = '';
 
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     //2. create a div
     const phoneCard = document.createElement("div");
-    phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+    phoneCard.classList = `card bg-gray-100 shadow-xl p-4`;
     //3. set the innerHTML
     phoneCard.innerHTML = `
         <figure>
@@ -37,7 +39,16 @@ const displayPhones = (phones) => {
         `;
         //4. append child
         phoneContainer.appendChild(phoneCard);
+        
   });
 };
+
+// handle search button
+const handleSearch = () => {
+   const searchField = document.getElementById('search-field');
+   const searchText = searchField.value;
+   console.log(searchText);
+   loadPhone(searchText);
+}
 
 loadPhone();
