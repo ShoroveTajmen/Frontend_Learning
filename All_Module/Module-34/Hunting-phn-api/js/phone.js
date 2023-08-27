a2b4c1e170deecfca5d98629b8cb328f092ab7cf
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-28 05:01:38 +0600

    add show all button functionality

--- All_Module/Module-34/Hunting-phn-api/js/phone.js
+++ All_Module/Module-34/Hunting-phn-api/js/phone.js
@@ -1,14 +1,14 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
   // console.log(phones);
-  displayPhones(phones);
+  displayPhones(phones, isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isShowAll) => {
   // console.log(phones);
 
   //1. get the element
@@ -18,14 +18,17 @@ const displayPhones = (phones) => {
 
   //display show all button if there are more than 12 phones
   const showAllContainer = document.getElementById("show-all-container");
-  if (phones.length > 12) {
+  if (phones.length > 12 && !isShowAll) {
     showAllContainer.classList.remove("hidden");
   } else {
     showAllContainer.classList.add("hidden");
   }
+  console.log('is show all', isShowAll);
 
-  //display only first 12 phones
-  phones = phones.slice(0, 12);
+  //display only first 12 phones if not show All
+  if(!isShowAll){
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
     // console.log(phone);
@@ -56,12 +59,12 @@ const displayPhones = (phones) => {
 };
 
 // handle search button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   //    console.log(searchText);
-  loadPhone(searchText);
+  loadPhone(searchText, isShowAll);
 };
 
 const toggleLoadingSpinner = (isLoading) => {
@@ -72,5 +75,19 @@ const toggleLoadingSpinner = (isLoading) => {
     loadingSpinner.classList.add("hidden");
   }
 };
+
+//handle show all
+const handleShowAll = () => {
+    handleSearch(true);
+}
+
+
+
+
+
+
+
+
+
 
 // loadPhone();
