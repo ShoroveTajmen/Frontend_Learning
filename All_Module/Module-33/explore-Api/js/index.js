4ae1fc314852a8ffe0f1c9fef88356fe889da4bb
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-27 14:57:05 +0600

    crud operation

--- All_Module/Module-33/explore-Api/js/index.js
+++ All_Module/Module-33/explore-Api/js/index.js
@@ -17,3 +17,40 @@ function displayUsers2(data) {
     ul2.appendChild(li2);
   }
 }
+
+function deletePost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  });
+}
+
+function PatchPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "foo",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function postAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
--- All_Module/Module-33/explore-Api/js/post.js
+++ All_Module/Module-33/explore-Api/js/post.js
@@ -15,14 +15,15 @@ function displayPosts(posts) {
   const postsContainer = document.getElementById("posts-container");
   for (const post of posts) {
     const div = document.createElement("div");
-    div.classList.add('post')
+    div.classList.add("post");
     div.innerHTML = `
         <h4>User-${post.userId}</h4>
         <h5>Post: ${post.title} </h5>
         <p>Post Description:  ${post.body}</p>
         
         `;
-        postsContainer.appendChild(div);
+    postsContainer.appendChild(div);
   }
 }
 loadPosts();
+
