a17f0a74f66e27559dea1f3ee3e036d6b8dcb19b
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-04 01:04:33 +0600

    alert confirm prompt

---
+++ All_Module/Module-36.6/promt-explore/js/promt.js
@@ -0,0 +1,19 @@
+const alertUser = () => {
+    alert('be attentive')
+}
+
+const SeriousStudent = () => {
+    const ans = confirm('seriioussss??????');
+    console.log(ans);
+
+    if(ans === true){
+        alert('well done');
+    }else{
+        console.log('valo hye jaw');
+    }
+}
+
+const getName = () => {
+    const name = prompt('WHAT IS YOUR NAME??');
+    console.log(name);
+}
