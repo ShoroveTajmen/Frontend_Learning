50c669fc066a1b0035194ce1bc1826aa27a73833
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-06 03:08:28 +0600

    dot and bracket notation

---
+++ All_Module/Moduule-37/js-fundamentals/notation.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'Lal nil holuud mia',
+    profession: 'Traffic surgeon',
+    age: 39,
+    address: 'shahabag'
+}
+
+// dot notation
+const prof1 = person.profession;
+
+//bracket notation
+const prof2 = person['profession'];
+const pName = 'profession';
+const prof33 = person[pName]
