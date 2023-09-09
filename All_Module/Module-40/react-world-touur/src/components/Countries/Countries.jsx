2ada0866df2884db11c3081fca52fc6af29e4b28
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-10 04:32:11 +0600

    toggle state based on click and consitional rending

--- All_Module/Module-40/react-world-touur/src/components/Countries/Countries.jsx
+++ All_Module/Module-40/react-world-touur/src/components/Countries/Countries.jsx
@@ -1,29 +1,29 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
-      //For data storing need useState() --> it returns an array
+  //For data storing need useState() --> it returns an array
   const [countries, setCountries] = useState([]);
 
   //for data loading useEffect
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-  }, [])
-
-
-
-    return (
-        <div>
-            <h3>Countries: {countries.length}</h3>
-            {
-                countries.map(country => <Country key={countries.cca3} country={country}></Country>)
-            }
-        </div>
-    );
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={countries.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
