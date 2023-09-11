2fdc5c8ae100e4b2de5dbec0a9737552126e9413
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-12 03:46:19 +0600

    add bookmark button and lift up the state

--- All_Module/Module-42/react-knowledge-cafe/src/components/Blog/Blog.jsx
+++ All_Module/Module-42/react-knowledge-cafe/src/components/Blog/Blog.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
   const { title, cover, author_img, author, posted_date, reading_time,
      hashtags } = blog;
   return (
-    <div>
-      <img src={cover} alt={`cover picture ${title}`} />
-      <div className="flex justify-between">
+    <div className="mb-20">
+      <img className="w-full mb-8" src={cover} alt={`cover picture ${title}`} />
+      <div className="flex justify-between mb-4">
         <div className="flex ">
           <img className="w-14" src={author_img} alt="" />
           <div className="ml-6">
@@ -15,7 +16,8 @@ const Blog = ({ blog }) => {
           </div>
         </div>
         <div>
-          <span>{reading_time} min read</span>
+          <span>{reading_time} mins read</span>
+          <button onClick={handleAddToBookmark} className="ml-2 text-red-600 text-2xl"><FaBookmark></FaBookmark></button>
         </div>
       </div>
       <h2 className="text-4xl">{title}</h2>
