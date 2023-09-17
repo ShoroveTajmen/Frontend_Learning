fe5c040e909324a57ca73012fbe3314f69896208
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-17 22:03:58 +0600

    create nav toggle and navbar animation

--- All_Module/Module-43/price-options/src/components/NavBar/NavBar.jsx
+++ All_Module/Module-43/price-options/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Link from "../Linkk/Link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { path: "/", name: "Home", id: "home" },
     { path: "/about", name: "About", id: "about" },
@@ -11,6 +14,13 @@ const NavBar = () => {
 
   return (
     <nav>
+      <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
+        {open === true ? (
+          <AiOutlineClose></AiOutlineClose>
+        ) : (
+          <AiOutlineMenu></AiOutlineMenu>
+        )}
+      </div>
       <ul className="md:flex">
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
