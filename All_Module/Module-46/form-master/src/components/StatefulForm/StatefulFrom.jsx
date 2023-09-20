4ade5d0e908cf1bc70c2ce0be49fa66e88c2ffad
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-21 01:29:41 +0600

    form with state to get value and set default value

---
+++ All_Module/Module-46/form-master/src/components/StatefulForm/StatefulFrom.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const StatefulFrom = () => {
+  const [name, setName] = useState("srvi");
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password.length < 6) {
+      setError("Password must be 6 characters or longer");
+    } else {
+      setError("");
+      console.log(email, password, name);
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    // console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={name}
+          onChange={handleNameChange}
+          type="text"
+          name="name"
+        />
+        <br />
+        <input onChange={handleEmailChange} type="email" name="email" id="" />
+        <br />
+        <input
+          required
+          onChange={handlePasswordChange}
+          type="password"
+          name="password"
+        />
+        <br />
+        <input type="submit" value="Submit" />
+        {
+            error && <p>{error}</p>
+        }
+      </form>
+    </div>
+  );
+};
+
+export default StatefulFrom;
