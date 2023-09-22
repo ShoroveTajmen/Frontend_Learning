3ec020a03a4f604699ac3fb7b65271eaa0e23333
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-22 19:09:24 +0600

    add icons and handle show all jobs

--- All_Module/Module-47/rect-creer-hub/src/Components/FeatureJobs/FeatureJobs.jsx
+++ All_Module/Module-47/rect-creer-hub/src/Components/FeatureJobs/FeatureJobs.jsx
@@ -4,6 +4,8 @@ import Job from "../Job/Job";
 
 const FeatureJobs = () => {
     const [jobs, setJobs] = useState([]);
+    //this is not the best way to load show all data
+    const [dataLength, setDataLenggth] = useState(4);
 
     useEffect(()=>{
         fetch('jobs.json')
@@ -21,8 +23,11 @@ const FeatureJobs = () => {
             </div>
             <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength === jobs.length ? 'hidden' : ''}>
+                <button onClick={()=> setDataLenggth(jobs.length)} className="btn btn-primary">Show All Jobs</button>
             </div>
         </div>
     );
--- All_Module/Module-47/rect-creer-hub/src/Components/Job/Job.jsx
+++ All_Module/Module-47/rect-creer-hub/src/Components/Job/Job.jsx
@@ -1,3 +1,5 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiFillDollarCircle } from "react-icons/ai";
 const Job = ({ job }) => {
   const { logo, job_title,company_name,
     remote_or_onsite,location,job_type,
@@ -16,6 +18,10 @@ const Job = ({ job }) => {
         <div>
             <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
             <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
+        </div>
+        <div className="mt-4 flex ">
+            <h2 className="flex mr-4"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+            <h2 className="flex"><AiFillDollarCircle className="text-2xl mr-2"></AiFillDollarCircle>{salary}</h2>
         </div>
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
