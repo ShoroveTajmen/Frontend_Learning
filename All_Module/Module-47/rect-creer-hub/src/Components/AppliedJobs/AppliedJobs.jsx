77ff6479730bff1842a7b55ff33c39e82adc8c60
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-23 02:51:43 +0600

    implement job filter and deploy to netlify and surge

--- All_Module/Module-47/rect-creer-hub/src/Components/AppliedJobs/AppliedJobs.jsx
+++ All_Module/Module-47/rect-creer-hub/src/Components/AppliedJobs/AppliedJobs.jsx
@@ -7,6 +7,21 @@ const AppliedJobs = () => {
   const jobs = useLoaderData();
 
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = filter => {
+    if(filter === 'all'){
+        setDisplayJobs(appliedJobs);
+    }
+    else if(filter === 'remote'){
+        const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+        setDisplayJobs(remoteJobs);
+    }
+    else if(filter === 'onsite'){
+        const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+        setDisplayJobs(onsiteJobs);
+    }
+  }
 
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
@@ -21,27 +36,28 @@ const AppliedJobs = () => {
         }
       }
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
     }
-  }, []);
+  }, [jobs]);
   return (
     <div>
       <h2 className="text-2xl">Jobs I applied: {appliedJobs.length}</h2>
       <details className="dropdown mb-32">
         <summary className="m-1 btn">open or close</summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-          <li>
-            <a>All</a>
+          <li onClick={() => handleJobsFilter('all')}>
+            <a >All</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter('remote')}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter('onsite')}>
             <a>On-Site</a>
           </li>
         </ul>
       </details>
       <ul>
-        {appliedJobs.map((job) => (
+        {displayJobs.map((job) => (
           <li key={job.id}>
             <span>
               {job.job_title} {job.company_name}: {job.remote_or_onsite}
