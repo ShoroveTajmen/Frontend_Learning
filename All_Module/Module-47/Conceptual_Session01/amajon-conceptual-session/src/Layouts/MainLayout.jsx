061ae7acf7608d5130b070358245d1f703f9e2b9
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-23 06:44:24 +0600

    footer added

--- All_Module/Module-47/Conceptual_Session01/amajon-conceptual-session/src/Layouts/MainLayout.jsx
+++ All_Module/Module-47/Conceptual_Session01/amajon-conceptual-session/src/Layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Footer/Footer";
 
 const MainLayout = () => {
   return (
@@ -6,20 +7,23 @@ const MainLayout = () => {
       <section className="flex justify-between px-10 shadow-md py-7">
         <h1 className="text-xl font-bold">Amajhon</h1>
         <nav>
-            <ul className="flex gap-5">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/products">Products</a>
-                </li>
-                <li>
-                    <a href="/dashboard">Dashboard</a>
-                </li>
-            </ul>
+          <ul className="flex gap-5">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/products">Products</a>
+            </li>
+            <li>
+              <a href="/dashboard">Dashboard</a>
+            </li>
+          </ul>
         </nav>
       </section>
+      <div className="min-h-screen">
         <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
