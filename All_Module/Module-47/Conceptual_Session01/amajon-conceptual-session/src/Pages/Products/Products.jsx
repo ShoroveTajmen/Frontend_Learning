91b8eb6b7e507b62d44ce46d3c55f4dc47423ef8
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-23 07:27:27 +0600

    fetch products data and showing in the product page

--- All_Module/Module-47/Conceptual_Session01/amajon-conceptual-session/src/Pages/Products/Products.jsx
+++ All_Module/Module-47/Conceptual_Session01/amajon-conceptual-session/src/Pages/Products/Products.jsx
@@ -1,9 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import ProductCards from "./ProductCards";
 
 
 const Products = () => {
+    const {products} = useLoaderData();
+    console.log(products);
     return (
-        <div>
-            <div>Products Page</div>
+        <div className="grid grid-cols-3 gap-5 p-5">
+            {
+                products.map(product => <ProductCards key={product.id} product={product}></ProductCards>)
+            }
         </div>
     );
 };
