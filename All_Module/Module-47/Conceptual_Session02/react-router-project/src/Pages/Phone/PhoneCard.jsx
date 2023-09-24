346d41cd116331be3f2d870a52e82a97c741d0b9
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-24 08:04:01 +0600

    set value in localStorage

--- All_Module/Module-47/Conceptual_Session02/react-router-project/src/Pages/Phone/PhoneCard.jsx
+++ All_Module/Module-47/Conceptual_Session02/react-router-project/src/Pages/Phone/PhoneCard.jsx
@@ -1,21 +1,35 @@
-const PhoneCard = ({ phone }) => {
+import swal from "sweetalert";
 
+const PhoneCard = ({ phone }) => {
   const { id, phone_name, brand_name, rating, price, image } = phone || {};
 
+  const handleAddToFavourites = () => {
+    const addedFavouritesArray = [];
+    const favoutiteItem = JSON.parse(localStorage.getItem("favourite"));
 
-    const handleAddToFavourites = () => {
-        console.log('clickedd')
+    //jokhon localstorage e kisu nai tokhon if er vhitore dhukbe
+    if (!favoutiteItem) {
+      addedFavouritesArray.push(phone);
+      localStorage.setItem("favourite", JSON.stringify(addedFavouritesArray));
+      swal("Good job!", "Products added successfully", "success");
+    } else {
+      const isExit = favoutiteItem.find((phone) => phone.id === id);
+      //jodi ei id ta already localstorage e na thake tahole add hobe
+      if (!isExit) {
+        addedFavouritesArray.push(...favoutiteItem, phone);
+        localStorage.setItem("favourite", JSON.stringify(addedFavouritesArray));
+        swal("Good job!", "Products added successfully", "success");
+      }else{
+        swal("Error!", "No duplicate", "error");
+      }
     }
-
+  };
 
   return (
     <div className="flex justify-center items-center h-[80vh]">
       <div className="relative flex w-full max-w-[48rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
         <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
-          <img
-            src= {image}
-            className="h-full w-full object-cover"
-          />
+          <img src={image} className="h-full w-full object-cover" />
         </div>
         <div className="p-6">
           <h6 className="mb-4 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
@@ -25,7 +39,8 @@ const PhoneCard = ({ phone }) => {
             {phone_name}
           </h4>
           <a className="inline-block" href="#">
-            <button onClick={handleAddToFavourites}
+            <button
+              onClick={handleAddToFavourites}
               className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
             >
