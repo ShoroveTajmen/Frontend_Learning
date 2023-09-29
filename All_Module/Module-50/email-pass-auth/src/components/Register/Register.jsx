17bbc161ecb865eadbdd188c085334d82a9c57ce
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-29 11:46:06 +0600

    simple form, and daisy ui form

--- All_Module/Module-50/email-pass-auth/src/components/Register/Register.jsx
+++ All_Module/Module-50/email-pass-auth/src/components/Register/Register.jsx
@@ -1,11 +1,28 @@
-
-
 const Register = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">Please Register</h2>
-        </div>
-    );
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+    }
+
+
+
+  return (
+    <div className="">
+      <div className="mx-auto md:w-1/2">
+        <h2 className="text-3xl mb-8">Please Register</h2>
+        <form onSubmit={handleRegister}>
+          <input className="mb-4 w-3/4 py-2 px-4 border-2 rounded-lg border-blue-500" type="email" name="email" id="" placeholder="Email Address"/>
+          <br />
+          <input className="mb-4 w-3/4 py-2 px-4 border-2 rounded-lg border-blue-500" type="password" name="password" id="" placeholder="Your Password"/>
+          <br />
+          <input className="btn btn-secondary mb-4 w-3/4" type="submit" name="Register" id="" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
