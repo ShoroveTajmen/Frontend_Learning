7ef6a9e4b7fb4316d6b797b9edab8162ff88970f
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-03 21:07:10 +0600

    first comment

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,54 @@
 import Navbar from "../Shared/Navbar/Navbar";
 
-
 const Login = () => {
-    return (
-        
-        <div>
-            <Navbar></Navbar>
-            <h2 className="text-3xl">This is login</h2>
-        </div>
-    );
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form)
+    }
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <h2 className="text-3xl my-10 text-center">This is login</h2>
+        <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
