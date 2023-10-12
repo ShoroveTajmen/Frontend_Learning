e5ce56f61fc9316e60460c5bfeb1b0e67319ab84
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-12 12:16:38 +0600

    delete a user from the database using UI click button

--- All_Module/Module-55/simple-crud-server/index.js
+++ All_Module/Module-55/simple-crud-server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 const corsOptions ={
@@ -48,6 +48,14 @@ async function run() {
         console.log('new user', user);
         const result = await userCollection.insertOne(user);
         res.send(result);
+    })
+
+    app.delete('/users/:id', async(req,res) => {
+        const id = req.params.id;
+        console.log('please delete from database', id);
+        const query = {_id : new ObjectId (id)}
+        const result = await userCollection.deleteOne(query);
+        res.send(result)
     })
 
     // Send a ping to confirm a successful connection
