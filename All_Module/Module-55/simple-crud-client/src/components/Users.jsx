de6627b289c36e4be2d9d84686ae0b00b385489b
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-12 12:15:52 +0600

    add delete functionaliity

--- All_Module/Module-55/simple-crud-client/src/components/Users.jsx
+++ All_Module/Module-55/simple-crud-client/src/components/Users.jsx
@@ -2,13 +2,29 @@ import { useLoaderData } from "react-router-dom";
 
 
 const Users = () => {
+
     const users = useLoaderData();
+
+    const handleDeletUser = (_id) => {
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.deletedCount > 0){
+                alert('deleted suuccessfully');
+            }
+        })
+    }
+
     return (
         <div>
             <h2>{users.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                    users.map(user => <p key={user._id}>{user.name} : {user.email} <button onClick={()=>handleDeletUser(user._id)}>X</button></p>)
                 }
             </div>
         </div>
