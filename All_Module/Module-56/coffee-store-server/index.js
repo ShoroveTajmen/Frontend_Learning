e49e711b3697b3eda3a1d584843dc2367b3ba818
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-13 19:21:12 +0600

    connect mongodb database

--- All_Module/Module-56/coffee-store-server/index.js
+++ All_Module/Module-56/coffee-store-server/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,11 +8,48 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//coffee
+//CXkAgI3e0SDj46Ka
 
-app.get('/', (req,res)=>{
-    res.send('Coffee making server is running')
-})
+
+// const uri =
+//   "mongodb+srv://<username>:<password>@cluster0.es62grd.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://coffee:CXkAgI3e0SDj46Ka@cluster0.es62grd.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Coffee making server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Coffee server is runningg on port: ${port}`)
-})
+  console.log(`Coffee server is runningg on port: ${port}`);
+});
