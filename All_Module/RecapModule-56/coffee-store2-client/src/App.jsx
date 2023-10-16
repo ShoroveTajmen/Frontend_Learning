422d0c9b55e7dff6b08fe3bbe63178ceadafef58
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-16 18:48:18 +0600

    view all cofffee and create coffee card

--- All_Module/RecapModule-56/coffee-store2-client/src/App.jsx
+++ All_Module/RecapModule-56/coffee-store2-client/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useLoaderData } from "react-router-dom";
+import CoffeeCard from "./Components/CoffeeCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const coffees = useLoaderData();
 
   return (
-    <>
-      <h1 className='text-6xl text-purple-600'>Vite + React</h1>
-
-    </>
-  )
+    <div className="m-10">
+      <h1 className="text-6xl text-center my-20 text-purple-600">
+        Hot Hot Cold Coffee: {coffees.length}
+      </h1>
+      <div className="grid grid-cols-2 gap-4">
+        {coffees.map((coffee) => (
+          <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- All_Module/RecapModule-56/coffee-store2-client/src/Components/AddCoffee.jsx
+++ All_Module/RecapModule-56/coffee-store2-client/src/Components/AddCoffee.jsx
@@ -1,19 +1,49 @@
+import Swal from "sweetalert2";
 const AddCoffee = () => {
-    const handleAddCoffee = event => {
-        event.preventDefault();
+  const handleAddCoffee = (event) => {
+    event.preventDefault();
 
-        const form = event.target;
-        const name = form.name.value
-        const quantity = form.quantity.value
-        const supplier = form.supplier.value
-        const taste = form.taste.value
-        const category = form.category.value
-        const details = form.details.value
-        const photo = form.photo.value
-        
-        const newCoffee = {name, quantity, supplier, taste, category, details, photo}
-        console.log(newCoffee)
-    }
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
+    console.log(newCoffee);
+
+    //send data to the server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "User added successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h2 className="text-3xl font-extrabold">AddCoffee</h2>
@@ -121,8 +151,11 @@ const AddCoffee = () => {
             </label>
           </div>
         </div>
-        <input type="submit" value="Add Coffee" className="btn btn-block bg-black text-white" />
-
+        <input
+          type="submit"
+          value="Add Coffee"
+          className="btn btn-block bg-black text-white"
+        />
       </form>
     </div>
   );
