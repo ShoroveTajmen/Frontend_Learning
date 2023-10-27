a2875ead71598205b6c6b9f15dab5bd892af120e
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-27 22:44:13 +0600

    extract booking service data

--- All_Module/Module-58/car-doctor-client/src/Pages/CheckOut/CheckOut.jsx
+++ All_Module/Module-58/car-doctor-client/src/Pages/CheckOut/CheckOut.jsx
@@ -1,20 +1,67 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const CheckOut = () => {
   const service = useLoaderData();
-  const { title, _id } = service;
+  const { title, _id, price } = service;
+  const { user } = useContext(AuthContext);
+
+  const handleBookService = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = user?.email;
+    const order = {
+      customerName: name,
+      email,
+      date,
+      service: _id,
+      price: price,
+    };
+    console.log(order);
+  };
+
   return (
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-12 ml-[450px]">
-      <form>
+    <div className="p-12">
+      <form onSubmit={handleBookService}>
         {" "}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {" "}
           <div className="form-control">
             <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              defaultValue={user?.displayName}
+              placeholder="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Date</span>
+            </label>
+            <input
+              type="date"
+              name="date"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
               type="email"
+              name="email"
+              defaultValue={user?.email}
               placeholder="email"
               className="input input-bordered"
               required
@@ -22,46 +69,24 @@ const CheckOut = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Password</span>
+              <span className="label-text">Due Amount</span>
             </label>
             <input
-              type="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
+              type="text"
+              defaultValue={"$" + price}
               className="input input-bordered"
               required
             />
           </div>
         </div>
+        <div className="form-control mt-6">
+          <input
+            className="btn btn-primary btn-block"
+            type="submit"
+            value="Order Confirmed"
+          />
+        </div>
       </form>
-      <div className="form-control mt-6">
-        <input
-          className="btn btn-primary btn-block"
-          type="submit"
-          value="Order Confirmed"
-        />
-      </div>
     </div>
   );
 };
