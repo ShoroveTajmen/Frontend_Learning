e8e25af0e195357c55e91ff846fe9649953c68b7
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-29 00:50:28 +0600

    jwt token secret and set token to http only cookie

---
+++ All_Module/Module-58/car-doctor-server/__jwt__steps__.js
@@ -0,0 +1,22 @@
+/***
+ * install jsonwebtoken
+ * jwt.sign(payload, secret, {exxpiresIn:})
+ * token client
+ */
+
+
+/***
+ * how to store token in the client side
+ * --------------------------------------
+ * memory --> ok type
+ * local storage --> ok type (xss)
+ * cookies: http only
+ */
+
+
+/***
+ * set cookies with http only
+ * for development secure: false,
+ * cors setting
+ * client side axios setting
+ */
--- All_Module/Module-58/car-doctor-server/index.js
+++ All_Module/Module-58/car-doctor-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -15,9 +16,7 @@ app.use(cors(corsOptions));
 //access post body and convert into json format
 app.use(express.json());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.es62grd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.es62grd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,71 +32,86 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const serviceCollection = client.db('carDoctor').collection('services');
-    const bookingCollection = client.db('carDoctor').collection('bookings');
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
 
+    //auth related API
+    app.post('/jwt', async(req, res)=> {{
+        const user = req.body;
+        console.log(user);
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        
+        res
+        .cookie('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'none'
+        })
+        .send({success: true})
+    }})
+
+
+    //services related API
     //get all data from database
-    app.get('/services', async(req, res) => {
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/services/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-        const options = {
-            // Include only the `title` and `imdb` fields in the returned document
-            projection: { title: 1, price: 1, service_id:1, img: 1 },
-          };
-        const result = await serviceCollection.findOne(query, options);
-        res.send(result)
-    })
-
-
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
 
     // *-----------
     // BOOKKINGS
     // --------------*
 
-    app.get('/bookings', async(req, res) => {
-        console.log(req.query.email);
-        let query = {};
-        if (req.query?.email){
-            query = {email: req.query.email}
-        }
-        const result = await bookingCollection.find(query).toArray();
-        res.send(result);
-    })
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
 
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
 
-    app.post('/bookings', async(req, res) => {
-        const booking = req.body;
-        console.log(booking);
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result);
-    })
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-
-    app.put('/bookings/:id', async(req, res) => {
-        const updatedBooking = req.body;
-    })
-
-
-    app.delete('/bookings/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await bookingCollection.deleteOne(query);
-        res.send(result);
-    })
-    
-
-
-
-
-
-
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
