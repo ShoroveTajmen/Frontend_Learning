abfc92903a3dd94c1f12878cfbba56e33bda548b
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-01 01:56:10 +0600

    jwt token verify and status code 402,403

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -14,6 +15,7 @@ const corsOptions = {
   app.use(cors(corsOptions));
   //access post body and convert into json format
   app.use(express.json());
+  app.use(cookieParser());
 
 
 console.log(process.env.DB_PASS)
@@ -29,6 +31,33 @@ const client = new MongoClient(uri, {
     }
 });
 
+//middlewares
+const logger = (req, res, next) => {
+    console.log('log: info',req.method, req.url);
+    next();
+}
+
+//verify middleware
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('token in the middleware', token);
+    //if no token is available
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user = decoded;
+        next();
+    })
+  
+}
+
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -39,7 +68,7 @@ async function run() {
 
         //Auth related API
         //for sign in
-        app.post('/jwt', async(req, res) => {
+        app.post('/jwt', logger, async(req, res) => {
             const user = req.body;
             console.log('user for token', user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'})
@@ -84,8 +113,12 @@ async function run() {
 
 
         // bookings 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', logger, verifyToken, async (req, res) => {
             console.log(req.query.email);
+            console.log('token owner info', req.user)
+            if(req.user.email !== req.query.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
