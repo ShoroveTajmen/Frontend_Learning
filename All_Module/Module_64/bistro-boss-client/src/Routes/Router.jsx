6723c0fac851adb17fd028a41eab308871f4f028
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-09 22:53:45 +0600

    add basic requirements and footer and navbar

---
+++ All_Module/Module_64/bistro-boss-client/src/Routes/Router.jsx
@@ -0,0 +1,20 @@
+
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Home from "../Pages/Homee/Home/Home";
+import Main from "../Layout/Main";
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
+    },
+  ]);
