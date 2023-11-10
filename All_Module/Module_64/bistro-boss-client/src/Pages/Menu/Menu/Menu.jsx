6fd8897ec0255fae404f6273e39cbb355b67b040
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-11 05:20:27 +0600

    display different menu category items with over

--- All_Module/Module_64/bistro-boss-client/src/Pages/Menu/Menu/Menu.jsx
+++ All_Module/Module_64/bistro-boss-client/src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,40 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/banner3.jpg';
-
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import saladImg from '../../../assets/menu/salad-bg.jpg';
+import soupImg from '../../../assets/menu/soup-bg.jpg';
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+const [menu] = useMenu();
+const desserts = menu.filter(item => item.category === 'dessert');
+const soup = menu.filter(item => item.category === 'soup');
+const salad = menu.filter(item => item.category === 'salad');
+const pizza = menu.filter(item => item.category === 'pizza');
+const offered = menu.filter(item => item.category === 'offered');
+
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="OUR MENU"></Cover>
+      {/* main cover */}
+      <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+      {/* offered menu items */}
+      <MenuCategory items={offered}></MenuCategory>
+      {/* dessert menu items */}
+      <MenuCategory items={desserts} title="Desserts" img={dessertImg}></MenuCategory>
+      {/* Pizza menu items */}
+      <MenuCategory items={pizza} title="Pizza" img={pizzaImg}></MenuCategory>
+      {/* salads menu items */}
+      <MenuCategory items={salad} title="Salads" img={saladImg}></MenuCategory>
+      {/* soups menu items */}
+      <MenuCategory items={ soup} title="Soups" img={soupImg}></MenuCategory>
     </div>
   );
 };
