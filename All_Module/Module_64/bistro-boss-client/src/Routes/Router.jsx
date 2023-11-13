01c2b0770043c5d7edb513dc398fbc1d89235ee0
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-13 15:17:05 +0600

    module introoduction and simple login form

--- All_Module/Module_64/bistro-boss-client/src/Routes/Router.jsx
+++ All_Module/Module_64/bistro-boss-client/src/Routes/Router.jsx
@@ -6,6 +6,7 @@ import Home from "../Pages/Homee/Home/Home";
 import Main from "../Layout/Main";
 import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
+import Login from "../Pages/Login/Login";
 
 
 export const router = createBrowserRouter([
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
         {
           path: '/order/:category',
           element: <Order></Order>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
         }
       ]
     },
