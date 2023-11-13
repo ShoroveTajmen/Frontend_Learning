3966eb96bbd4ccc658865ff6c718608ceccd5698
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-13 16:27:15 +0600

    firebase project setup for bistro boss

--- All_Module/Module_64/bistro-boss-client/src/main.jsx
+++ All_Module/Module_64/bistro-boss-client/src/main.jsx
@@ -4,14 +4,18 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Router.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Provider/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
+    <AuthProvider>
       {" "}
-      <div className="max-w-screen-xl mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+      <HelmetProvider>
+        {" "}
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
