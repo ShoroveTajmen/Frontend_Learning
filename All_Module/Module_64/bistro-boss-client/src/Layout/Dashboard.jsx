9a13811ecf20bc6b16168c3b0571a1cf8fed6e3e
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-14 18:50:11 +0600

    cart data from custom hook and calculate total price

--- All_Module/Module_64/bistro-boss-client/src/Layout/Dashboard.jsx
+++ All_Module/Module_64/bistro-boss-client/src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
@@ -37,10 +37,23 @@ const Dashboard = () => {
               <FaList></FaList>My Bookings
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              {" "}
+              <FaHome></FaHome>Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              {" "}
+              <FaSearch></FaSearch>Menu
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
