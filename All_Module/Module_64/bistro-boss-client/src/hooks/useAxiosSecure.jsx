51629bcf36e56cbe540ddfc9b65b2a38afd5b3c1
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-16 13:20:03 +0600

    log out unauthorized access and check is admin

--- All_Module/Module_64/bistro-boss-client/src/hooks/useAxiosSecure.jsx
+++ All_Module/Module_64/bistro-boss-client/src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const {logOutUser} = useAuth();
     //request interceptor to add authorization header for every secure call to the api
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token')
@@ -18,9 +22,14 @@ const useAxiosSecure = () => {
     //intercepts 401 and 403 status
     axiosSecure.interceptors.response.use(function(response){
         return response;
-    }, (error)=> {
+    }, async(error)=> {
         const status = error.response.status;
-        console.log('status error in the interceptors', status)
+        console.log('status error in the interceptors', status);
+        //for 401 or 403 logout the user and move the user to the login page
+        if(status === 401 || status === 403){
+            await logOutUser();
+            navigate('/login')
+        }
         return Promise.reject(error);
     })
 
