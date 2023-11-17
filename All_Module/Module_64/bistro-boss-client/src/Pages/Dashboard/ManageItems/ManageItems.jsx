7b22ed38c0e44d138e6e80b12f98f4c7dab0c235
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-17 13:57:30 +0600

    delete menu item with verifyAdmin middleware

--- All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,11 +1,45 @@
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
-  const [menu] = useMenu();
+  const [menu, loading, refetch] = useMenu();
+  const axiosSecure = useAxiosSecure();
 
-  const handleDeleteItem = (item) => {};
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/menu/${item._id}`);
+        // console.log(res.data);
+        if (res.data.deletedCount > 0) {
+          //refetch to update the ui
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${item.name} has been deleted`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+      }
+    });
+  };
 
   return (
     <div>
@@ -46,9 +80,7 @@ const ManageItems = () => {
                   <td>{item.name}</td>
                   <td className="text-right">${item.price}</td>
                   <td>
-                    <button
-                      className="btn btn-ghost btn-lg bg-orange-500"
-                    >
+                    <button className="btn btn-ghost btn-lg bg-orange-500">
                       <FaEdit className="text-white "></FaEdit>
                     </button>
                   </td>
