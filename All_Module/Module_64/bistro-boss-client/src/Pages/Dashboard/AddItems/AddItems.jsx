f0404778665ed2263de2a11a40500ee8198a2a44
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-17 11:53:58 +0600

    save menu item too the server

--- All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/AddItems/AddItems.jsx
+++ All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,24 +2,51 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
-
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
 
   const onSubmit = async (data) => {
     console.log(data);
     //image upload to imgbb and then get an url
-    const imageFile = {image: data.image[0]}
+    const imageFile = { image: data.image[0] };
     const res = await axiosPublic.post(image_hosting_api, imageFile, {
       headers: {
-        'content-type' : 'multipart/form-data'
-      }
-    })
+        "content-type": "multipart/form-data",
+      },
+    });
     console.log(res.data);
+    if (res.data.success) {
+      //now send the menu item data to the server with the image url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      //
+      const menuRes = await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.insertedId) {
+        //show success popup
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} is added to the menu`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
   };
 
   return (
@@ -37,7 +64,7 @@ const AddItems = () => {
             <input
               type="text"
               placeholder="Reciepe Name"
-              {...register("name", {required: true})}
+              {...register("name", { required: true })}
               required
               className="input input-bordered w-full "
             />
@@ -48,8 +75,9 @@ const AddItems = () => {
               <label className="label">
                 <span className="label-text">Category Name*</span>
               </label>
-              <select defaultValue="default"
-                {...register("category", {required: true})}
+              <select
+                defaultValue="default"
+                {...register("category", { required: true })}
                 className="select select-bordered w-full"
               >
                 <option disabled value="default">
@@ -70,7 +98,7 @@ const AddItems = () => {
               <input
                 type="number"
                 placeholder="Price"
-                {...register("price", {required: true})}
+                {...register("price", { required: true })}
                 className="input input-bordered w-full "
               />
             </div>
@@ -83,13 +111,19 @@ const AddItems = () => {
             <textarea
               className="textarea textarea-bordered h-24"
               placeholder="Bio"
-              {...register('recipe')}
+              {...register("recipe")}
             ></textarea>
           </div>
           <div className="form-control w-full my-6">
-            <input {...register('image', {required: true})} type="file" className="file-input w-full max-w-xs" />
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
           </div>
-          <button className="btn">Add Item <FaUtensils className="ml-4"></FaUtensils></button>
+          <button className="btn">
+            Add Item <FaUtensils className="ml-4"></FaUtensils>
+          </button>
         </form>
       </div>
     </div>
