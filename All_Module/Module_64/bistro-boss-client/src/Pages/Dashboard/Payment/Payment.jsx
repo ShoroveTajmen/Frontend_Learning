bdfc16d00e1183badd02b249fa5f6708f7dec46e
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-19 21:55:03 +0600

    create payment route

---
+++ All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
+            <div>
+                <h2 className="text-4xl">taka paymennnnnnnnnttttt</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
