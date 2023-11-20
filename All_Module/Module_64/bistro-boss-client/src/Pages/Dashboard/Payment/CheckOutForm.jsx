1a2011e840c721749f23f1d9d627c94de93a0976
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-20 17:33:39 +0600

    use confirm crd pyment to process pyment

--- All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -2,13 +2,16 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckOutForm = () => {
     const [error, setError] = useState('');
     const [clientSecret, setClientSecret] = useState('');
+    const [transactionId, setTransactionId] = useState('');
     const stripe = useStripe();
     const elements = useElements();
     const axiosSecure = useAxiosSecure();
+    const {user} = useAuth();
     const [cart] = useCart();
     const totalPrice = cart.reduce((total, item) => total + item.price, 0)
 
@@ -46,6 +49,26 @@ const CheckOutForm = () => {
         console.log('payment method', paymentMethod)
         setError('');
     }
+
+    //confirm payment
+    const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+            billing_details: {
+                email: user?.email || 'anonymous',
+                name: user?.displayName || 'anonymous'
+            }
+        }
+    })
+    if(confirmError){
+        console.log('confirm error')
+    }else{
+        console.log('payment intent', paymentIntent)
+        if(paymentIntent.status === 'succeeded'){
+            console.log('transaction id', paymentIntent.id);
+            setTransactionId(paymentIntent.id);
+        }
+    }
   };
 
   return (
@@ -70,6 +93,7 @@ const CheckOutForm = () => {
         Pay
       </button>
       <p className="text-red-600">{error}</p>
+      {transactionId && <p className="text-green-600">Your Transaction id: {transactionId}</p>}
     </form>
   );
 };
--- All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/Payment/__payment_steps_.js
+++ All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/Payment/__payment_steps_.js
@@ -3,4 +3,8 @@
  * 2. create crd element
  * 3. create stripe account and get publishable key
  * 4. use publishable kkey and use stripe to get card informaataaion and error
+ * 5. create payment intent post on the server. and return client secret. install stripe on the server side and get client secret.make sure you used the payment method types: ['card']
+ * 6. from client side get the client secret and save it.
+ * 7. use confirm card payment and pass user information, card and client secret
+ * 8. display transaction id
  */
