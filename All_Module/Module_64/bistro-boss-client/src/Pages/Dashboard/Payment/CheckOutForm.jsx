d9cd70004d418119b03de4d45cb73eaa7368ac9e
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-20 16:51:18 +0600

    send price and get client secret from stripe

--- All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ All_Module/Module_64/bistro-boss-client/src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,10 +1,24 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
 
 const CheckOutForm = () => {
     const [error, setError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSecure = useAxiosSecure();
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', {price: totalPrice})
+        .then(res => {
+            console.log(res.data.clientSecret)
+            setClientSecret(res.data.clientSecret)
+        })
+    },[axiosSecure, totalPrice])
 
 
   const handleSubmit = async (e) => {
@@ -52,7 +66,7 @@ const CheckOutForm = () => {
           },
         }}
       />
-      <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe}>
+      <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe || !clientSecret}>
         Pay
       </button>
       <p className="text-red-600">{error}</p>
